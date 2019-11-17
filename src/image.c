332c76ece900e1c6fec1a83733888ae3fbd74f8b
Victor Widell <victor@topmost.se>
2019-11-17 19:30:56 +0100

    Implemented per-pixel horizontal draw position.

--- src/image.c
+++ src/image.c
@@ -9,11 +9,29 @@ void freeImage (Image image) {
 void drawImage(Image image, unsigned int posX, unsigned int posY) {
     unsigned int y, column;
     long unsigned int strip;
+    unsigned int posXColumn, posXRest;
+    unsigned long int stripShiftedA, stripShiftedB;
+    unsigned long int shiftMask;
 
     for (y = 0; y < image.height; ++y) {
         for (column=0; column<image.numColumns; ++column) {
             strip = makeBitplaneStrip(image.pixels[column + (image.upsideDown ? (image.height - 1 - y) : y)*image.numColumns]);
-            drawStrip(column + posX/8, y + posY, strip, 0xFF);
+
+            posXColumn = posX/8;
+            posXRest = posX%8;
+
+            shiftMask =
+                (((unsigned long int) 0xff >> posXRest) << 24) |
+                (((unsigned long int) 0xff >> posXRest) << 16) |
+                (((unsigned long int) 0xff >> posXRest) << 8) |
+                (((unsigned long int) 0xff >> posXRest) << 0);
+
+            stripShiftedA = (strip >> posXRest) & shiftMask;
+            stripShiftedB = (strip << (8 - posXRest)) & (~shiftMask);
+
+            // TODO: Combine the adjecent strips, to avoid double writes.
+            drawStrip(column + posXColumn, y + posY, stripShiftedA, (shiftMask) & 0xff);
+            drawStrip(column + posXColumn + 1, y + posY, stripShiftedB, (~shiftMask) & 0xff);
         }
     }
 }
