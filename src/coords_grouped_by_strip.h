4267146e4ca2708938fb177a96aa74dfd0ab6bc8
Victor Widell <victor@topmost.se>
2020-07-13 21:08:53 +0200

    Made grouped table a generic class.

--- src/coords_grouped_by_strip.h
+++ src/coords_grouped_by_strip.h
@@ -1,33 +1,15 @@
 #ifndef coords_grouped_by_strip_h
 #define coords_grouped_by_strip_h
 
+#include "grouped_table.h"
 #include "vector.h"
 #include "platform/dos/bitplane_strip.h"
 #include "strip_coord.h"
 
 
 declareVector(StripCoordVector, StripCoord, stripCoordVector)
-
-
-typedef struct CoordsByStripTableRow {
-	BitplaneStrip key;
-	StripCoordVector values;
-} CoordsByStripTableRow;
-
-
+declareGroupedTable(CoordsByStripTable, BitplaneStrip, StripCoord, StripCoordVector, coordsByStripTable)
 declareVector(CoordsByStripTableRowsVector, CoordsByStripTableRow, coordsByStripTableRowsVector)
-
-
-typedef struct CoordsByStripTableStruct *CoordsByStripTable;
-
-
-CoordsByStripTable makeCoordsByStripTable(const int capacity);
-void initializeCoordsByStripTable(CoordsByStripTable table, const int capacity);
-void destroyCoordsByStripTable(CoordsByStripTable table);
-void freeCoordsByStripTable(CoordsByStripTable table);
-void coordsByStripTableAdd(CoordsByStripTable table, const BitplaneStrip key, const StripCoord value);
-CoordsByStripTableRow* coordsByStripTableBegin(CoordsByStripTable table);
-CoordsByStripTableRow* coordsByStripTableEnd(CoordsByStripTable table);
 
 
 #endif
