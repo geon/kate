af4c755450ae0cac6efdd3f6b81cb3cb61f0fffb
Victor Widell <victor@topmost.se>
2020-12-12 19:04:02 +0100

    Refactor. Moved variable to scope.

--- src/sprite.c
+++ src/sprite.c
@@ -7,8 +7,6 @@
 
 
 Sprite makeSprite (Image image) {
-	unsigned short int numBitplaneStrips = image->numColumns * image->height;
-
 	Sprite sprite = malloc(sizeof(SpriteStruct));
 
 	sprite->numColumns = image->numColumns;
@@ -16,19 +14,23 @@ Sprite makeSprite (Image image) {
 
 	memcpy(sprite->palette, image->palette, sizeof(sprite->palette));
 
-	sprite->bitPlaneStrips = malloc(sizeof(BitplaneStrip) * numBitplaneStrips);
-	sprite->mask = malloc(sizeof(unsigned char) * numBitplaneStrips);
 	{
-		unsigned int i;
-		for (i=0; i<numBitplaneStrips; ++i) {
-			unsigned short int column, y;
-			column = i % image->numColumns;
-			y = i / image->numColumns;
-			if (image->upsideDown) {
-				y = image->height - 1 - y;
+		unsigned short int numBitplaneStrips = image->numColumns * image->height;
+		sprite->bitPlaneStrips = malloc(sizeof(BitplaneStrip) * numBitplaneStrips);
+		sprite->mask = malloc(sizeof(unsigned char) * numBitplaneStrips);
+
+		{
+			unsigned int i;
+			for (i=0; i<numBitplaneStrips; ++i) {
+				unsigned short int column, y;
+				column = i % image->numColumns;
+				y = i / image->numColumns;
+				if (image->upsideDown) {
+					y = image->height - 1 - y;
+				}
+				sprite->bitPlaneStrips[i] = makeBitplaneStrip(image->pixels[column + y*image->numColumns]);
+				sprite->mask[i] = image->mask[column + y*image->numColumns];
 			}
-			sprite->bitPlaneStrips[i] = makeBitplaneStrip(image->pixels[column + y*image->numColumns]);
-			sprite->mask[i] = image->mask[column + y*image->numColumns];
 		}
 	}
 
