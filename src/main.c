8f36f885022427e634eba526e1819ed659295058
Victor Widell <victor@topmost.se>
2021-03-06 20:04:22 +0100

    Added FPS timer.

--- src/main.c
+++ src/main.c
@@ -5,11 +5,14 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <time.h>
+#include <math.h>
 
 #define NUM_FRAMES 100
 
 
 int16_t main (int16_t argc, char* argv[]) {
+	clock_t startTime, stopTime, deltaTime;
 	Renderer renderer;
 	World world;
 
@@ -37,15 +40,22 @@ int16_t main (int16_t argc, char* argv[]) {
 
 	{
 		uint16_t frame;
+		startTime = clock();
 		for (frame=0; frame<NUM_FRAMES; ++frame) {
 			worldUpdate(world);
 			worldRender(world);
 		}
+		stopTime = clock();
 	}
 
 	// Restore screen.
 	setBufferOffset(0, 0);
 	setVirtualScreenWidth(EGA_BUFFER_NUM_COLUMNS_DEFAULT);
+
+	deltaTime = stopTime - startTime;
+	if (deltaTime) {
+		printf("FPS: %i\n", NUM_FRAMES*CLOCKS_PER_SEC/deltaTime);
+	}
 
 	freeWorld(world);
 	freeRenderer(renderer);
